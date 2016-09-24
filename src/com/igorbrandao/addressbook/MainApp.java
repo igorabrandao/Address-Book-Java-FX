1ac54e5112d633a4013567b53c240be6759aeab2
Igor Brandão <igorabrandao@gmail.com>
2016-09-24 18:49:47 -0300

    TUTORIAL PART 2
    -Added the table view component;
    -Connected the interface with the code using observable list and properties as a binder.

--- src/com/igorbrandao/addressbook/MainApp.java
+++ src/com/igorbrandao/addressbook/MainApp.java
@@ -2,7 +2,12 @@ package com.igorbrandao.addressbook;
 
 import java.io.IOException;
 
+import com.igorbrandao.addressbook.model.Person;
+import com.igorbrandao.addressbook.view.PersonOverviewController;
+
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
@@ -14,6 +19,35 @@ public class MainApp extends Application {
     private Stage primaryStage;
     private BorderPane rootLayout;
 
+    /**
+     * Os dados como uma observable list de Persons.
+     */
+    private ObservableList<Person> personData = FXCollections.observableArrayList();
+
+    /**
+     * Construtor
+     */
+    public MainApp() {
+        // Add some sample data
+        personData.add(new Person("Hans", "Muster"));
+        personData.add(new Person("Ruth", "Mueller"));
+        personData.add(new Person("Heinz", "Kurz"));
+        personData.add(new Person("Cornelia", "Meier"));
+        personData.add(new Person("Werner", "Meyer"));
+        personData.add(new Person("Lydia", "Kunz"));
+        personData.add(new Person("Anna", "Best"));
+        personData.add(new Person("Stefan", "Meier"));
+        personData.add(new Person("Martin", "Mueller"));
+    }
+
+    /**
+     * Retorna os dados como uma observable list de Persons. 
+     * @return
+     */
+    public ObservableList<Person> getPersonData() {
+        return personData;
+    }
+    
     @Override
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
@@ -44,17 +78,22 @@ public class MainApp extends Application {
     }
 
     /**
-     * Mostra o person overview dentro do root layout.
+     * Mostra a person overview dentro do root layout.
      */
     public void showPersonOverview() {
         try {
-            // Carrega o person overview.
+            // Carrega a person overview.
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
             AnchorPane personOverview = (AnchorPane) loader.load();
 
-            // Define o person overview dentro do root layout.
+            // Define a person overview no centro do root layout.
             rootLayout.setCenter(personOverview);
+
+            // Dá ao controlador acesso à the main app.
+            PersonOverviewController controller = loader.getController();
+            controller.setMainApp(this);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
